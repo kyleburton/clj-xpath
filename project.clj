9d0fa26a1f9cd304207bf99752ece31ccdbf69fe
Kyle Burton <kburton@relaynetwork.com>
2012-05-12 15:42:45 -0400

    lein-release plugin: preparing 1.3.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kyleburton/clj-xpath "1.3.1-SNAPSHOT"
+(defproject org.clojars.kyleburton/clj-xpath "1.3.1"
   :description "Simplified XPath from Clojure."
   :url         "http://github.com/kyleburton/clj-xpath"
   :lein-release {:deploy-vai :clojars}
