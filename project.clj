7c41b190625ed788fa17b891d8de0851db69c1da
Kyle Burton <kyle.burton@gmail.com>
2012-11-14 09:55:44 -0500

    lein-release plugin: bumped version from 1.3.3 to 1.3.4-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kyleburton/clj-xpath "1.3.3"
+(defproject org.clojars.kyleburton/clj-xpath "1.3.4-SNAPSHOT"
   :description "Simplified XPath from Clojure."
   :url         "http://github.com/kyleburton/clj-xpath"
   :lein-release {:deploy-via :clojars}
