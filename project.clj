8cc55f9968b4a9a0c6e4674355c25ff5975bffab
Kyle Burton <kyle.burton@gmail.com>
2012-12-14 16:32:51 -0500

    upgrade swank-clojure to work with clojure 1.5.x

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :distribution :repo
             :comments "Same as Clojure"}
   :local-repo-classpath true
-  :dev-dependencies [[swank-clojure "1.4.2"]
+  :dev-dependencies [[swank-clojure "1.4.3"]
                      [midje "1.4.0"]]
   :dependencies [[org.clojure/clojure "1.5.0-alpha6"]
                  [xalan "2.7.1"]])
