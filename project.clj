d2fa5c8aa4430e5d9bc60b4bfcf21f4026293d29
Kyle Burton <kyle.burton@gmail.com>
2016-12-01 21:22:24 -0800

    move actual deps from dev deps to real deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.kyleburton/clj-xpath "1.4.8"
+(defproject com.github.kyleburton/clj-xpath "1.4.9-SNAPSHOT"
   :description          "Simplified XPath from Clojure."
   :url                  "http://github.com/kyleburton/clj-xpath"
   :license              {:name "Eclipse Public License - v 1.0"
@@ -10,9 +10,7 @@
   ;; :main ^:skip-aot clj-xpath.nrepl
   :plugins [[org.clojure/tools.nrepl   "0.2.11"]]
   :profiles             {:dev {:dependencies [[org.clojure/clojure       "1.8.0"]
-                                              [org.clojure/tools.logging "0.3.1"]
-                                              [cider/cider-nrepl         "0.10.2"]
-                                              [prismatic/schema          "1.0.5"]]}
+                                              [cider/cider-nrepl         "0.10.2"]]}
                          ;; NB: the use of ex-info prevents 1.3 from being supported
                          :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
                          :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
@@ -21,6 +19,7 @@
   :aliases              {"all" ["with-profile" "1.5:1.6:1.7"]}
   :global-vars          {*warn-on-reflection* true}
   :dependencies [
+    [org.clojure/tools.logging  "0.3.1"]
     [xalan                      "2.7.2"]
     [prismatic/schema           "1.1.3"]
   ])
